780bbfa222ed6685e1d7aecdfbb02b596773e2b6
Joshua Richardson <joshua.richardson11@icloud.com>
2020-07-31 21:55:12 -0700

    Add the Browserwindow

--- app/main.js
+++ app/main.js
@@ -1,6 +1,13 @@
-const { app } = require("electron");
+const { app, BrowserWindow } = require("electron");
+
+// Creates a variable in the top-level scope for the main window of our application
+let mainWindow = null;
 
 // Called as soon as the application has fully launched
 app.on("ready", () => {
   console.log("Hello from Electron");
+
+  // When the application is ready, creates a browser window, and assigns it to the variable
+  // created in the top-level scope.
+  mainWindow = new BrowserWindow();
 });
