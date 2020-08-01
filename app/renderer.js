cb0f36bb9a91e1e2d42283cc1241ae5100edc3a9
Joshua Richardson <joshua.richardson11@icloud.com>
2020-07-31 22:20:45 -0700

    Add functionality to enable the new link submit button if new link is a valid input.

--- app/renderer.js
+++ app/renderer.js
@@ -4,3 +4,10 @@ const newLinkForm = document.querySelector(".new-link-form");
 const newLinkUrl = document.querySelector(".new-link-url");
 const newLinkSubmit = document.querySelector(".new-link-submit");
 const clearStorageButton = document.querySelector(".clear-storage");
+
+newLinkUrl.addEventListener("keyup", () => {
+  // When a user types in the input field, this uses Chromium's ValidityState API
+  // to determine if the input is valid. If so, removes the disabled attribute
+  // from the submit button
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
