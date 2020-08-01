de37c75b7118d1b5e946f898365a68e57b337f3a
Joshua Richardson <joshua.richardson11@icloud.com>
2020-07-31 22:32:00 -0700

    Add functionality for storing bookmarks to local storage

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,6 @@
+// Creates a DOMParser instance. We use this after fetching the text contents from the provided url
+const parser = new DOMParser();
+
 const linksSection = document.querySelector(".links");
 const errorMessage = document.querySelector(".error-message");
 const newLinkForm = document.querySelector(".new-link-form");
@@ -12,7 +15,37 @@ newLinkUrl.addEventListener("keyup", () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
 });
 
+newLinkForm.addEventListener("submit", (event) => {
+  // Tells Chromium not to trigger an HTTP request (the default action for form submission)
+  event.preventDefault();
+
+  // Grabs the URL in the new link input field.
+  const url = newLinkUrl.value;
+
+  fetch(url) // Uses the fetch API to fetch the content of the provided url
+    .then((response) => response.text()) // parses the response as plain text
+    .then(parseResponse)
+    .then(findTitle)
+    .then(title => storeLink(title, url))
+    .then(clearForm)
+});
+
 // Clears the value of the new link input field by setting the value to null
 const clearForm = () => {
   newLinkUrl.value = null;
 };
+
+// Takes the string of HTML from the URL and parses into a DOM tree
+const parseResponse = (text) => {
+  return parser.parseFromString(text, "text/html");
+};
+
+// Traverses the DOM tree to find the <title> node
+const findTitle = (nodes) => {
+  return nodes.querySelector("title").innerText;
+};
+
+// Stores the title and URL into local storage
+const storeLink = (title, url) {
+  localStorage.setItem(url, JSON.stringify({ title, url}));
+}
